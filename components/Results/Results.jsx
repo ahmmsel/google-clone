5e904d0374cf0cd4e3197b1d707b5e2c0dd403e6
ali hussein mohamed <89816740+ahmmsel@users.noreply.github.com>
2022-06-30 18:46:16 +0200

    Update Results.jsx

--- components/Results/Results.jsx
+++ components/Results/Results.jsx
@@ -1,24 +1,26 @@
-import ResultsItem from "../ResultsItem/ResultsItem"
-import style from "./Results.module.scss"
+import ResultsItem from "../ResultsItem/ResultsItem";
+import style from "./Results.module.scss";
 
 export default function Results({ data }) {
-  const { formattedTotalResults, formattedSearchTime } = data.searchInformation
   return (
     <section>
       <div className="container">
-        <small>About {formattedTotalResults} results({formattedSearchTime} seconds)</small>
+        <small>
+          About {data?.searchInformation?.formattedTotalResults} results(
+          {data?.searchInformation?.formattedSearchTime} seconds)
+        </small>
         <div className={style.list}>
-          {data.items?.map((item, index) => (
-            <ResultsItem 
-              key={index} 
-              title={item.title} 
-              link={item.link} 
-              htmlSnippet={item.htmlSnippet} 
+          {data?.items?.map((item, index) => (
+            <ResultsItem
+              key={index}
+              title={item.title}
+              link={item.link}
+              htmlSnippet={item.htmlSnippet}
             />
           ))}
-          {!data.items && <p>Not Result Found</p>}
+          {!data?.items && <p>Not Result Found</p>}
         </div>
       </div>
     </section>
-  )
+  );
 }
